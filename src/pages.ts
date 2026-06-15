38a88139be152c19bace905ca8552470da9d70fb
David Renshaw <renshaw@icarm.io>
2026-06-15 09:28:00 -0400

    database -> website

--- src/pages.ts
+++ src/pages.ts
@@ -85,7 +85,7 @@ export function layout(title: string, bodyInner: string, user: User | null = nul
         <a class="external" href="https://github.com/icarm/elliptic-rank">source</a> &nbsp;&middot;&nbsp;
         <a class="external" href="https://icarm.io">icarm.io</a>
       </nav>
-      <p class="acknowledgment">This database is maintained by the NSF Institute for Computer-Aided
+      <p class="acknowledgment">This website is maintained by the NSF Institute for Computer-Aided
       Reasoning in Mathematics <span class="nowrap">(<a class="external" href="https://icarm.io">ICARM</a>)</span>. If you find it
       useful, please consider acknowledging ICARM and NSF Grant DMS 2425401 in related publications,
       projects, or other scholarly work.</p>
